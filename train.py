bb0d3a7657a56b3d643744db13f603c07e268c19
Jason Chitla <jasonchitla@hotmail.com>
2023-08-10 02:59:28 -0400

    change dropout values

--- train.py
+++ train.py
@@ -129,7 +129,7 @@ if __name__ == '__main__':
             'values': [1, 2]
         },
         'dropout': {
-            'values': [0.3, 0.4]
+            'values': [0.4, 0.45]
         }
     }
     sweep_id = wandb.sweep(sweep_config, project="harmonic-diffusion-antibodies")
